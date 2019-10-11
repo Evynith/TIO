0adbd64619117e4a99ff8fa8b6f7f703cbda90a4
DicoNet <sebastian.dico@hotmail.com>
2019-10-10 23:34:57 -0300

    se implementa ajax para guardar datos persistentes de la tabla, se solucionan bugs

--- js/javascript.js
+++ js/javascript.js
@@ -102,90 +102,253 @@ if(botonComprobar){
 botonComprobar.addEventListener("click", leerdato);}
 /*----------------------------------------------------------------------------------------------------------*/
 /*----------------------------------------------------------------------------------------------------------*/
-/*-----------------------------------------PARTE 2 TPE------------------------------------------------------*/
+/*-----------------------------------------PARTE 2-3 TPE------------------------------------------------------*/
 /*----------------------------------------------------------------------------------------------------------*/
 /*----------------------------------------------------------------------------------------------------------*/
-let planes = [{
-    "plan300": "Enfermedades Graves (ACV, Infarto o Cáncer) $100.000",
-    "plan600": "Enfermedades Graves (ACV, Infarto o Cáncer) $200.000",
-    "plan900": "Enfermedades Graves (ACV, Infarto o Cáncer) $300.000",
-},
-{
-    "plan300": "Transplante de Órgano $100.000",
-    "plan600": "Transplante de Órgano $200.000",
-    "plan900": "Transplante de Órgano $300.000",
+  let cuerpoTabla = document.querySelector("#CuerpoTabla");
+  let btn = document.querySelector("#BorrarTabla");
+  CrearCeldasSiHayValoresEnLaAPI();
+  if (btn) {
+    btn.addEventListener("click", borrarjson);
+  }
+  let btnEnviar = document.querySelector("#BtnEnviar");
+  if (btnEnviar) {
+    btnEnviar.addEventListener("click", AgregarValores);
+  }
+  let btn3test = document.querySelector("#Btn3tests");
+  if (btn3test) {
+    btn3test.addEventListener("click", Agregar3Valores);
+  }
 
-},
-{
-    "plan300": "Internación por Cirugías: (monto máximo por día)",
-    "plan600": "Internación por Cirugías: (monto máximo por día)",
-    "plan900": "Internación por Cirugías: (monto máximo por día)",
-}]
-let cuerpoTabla = document.querySelector("#CuerpoTabla");
-let btn = document.querySelector("#BorrarTabla");
-CrearCeldas(0, planes.length);
-if(btn){
-btn.addEventListener("click", borrarjson);}
-let btnEnviar = document.querySelector("#BtnEnviar");
-if(btnEnviar){
-btnEnviar.addEventListener("click", AgregarValores);}
-let btn3test = document.querySelector("#Btn3tests");
-if(btn3test){
-btn3test.addEventListener("click", Agregar3Valores);}
 
-function CrearCeldas(indice, final) {
+  document.addEventListener("DOMSubtreeModified",BtnBorrarFila);
+  document.addEventListener("DOMSubtreeModified",BtnEditarFila);
+  
+  
+  async function CrearCeldasSiHayValoresEnLaAPI() {
+    let r = await fetch('http://web-unicen.herokuapp.com/api/groups/07/salud8',
+      {
+        "method": "GET",
+        "headers": { "Content-Type": "application/json" },
+      });
+    let json = await r.json();
+    CrearCeldas(0, json.salud8.length);
+    
+
+
+
+  }
+  async function CrearCeldas(indice, final) {
+    let respuesta = await fetch('http://web-unicen.herokuapp.com/api/groups/07/salud8',
+      {
+        "method": "GET",
+        "headers": { "Content-Type": "application/json" },
+      });
+    let json2 = await respuesta.json();
     for (let i = indice; i < final; i++) {
-        if(cuerpoTabla){
+      if (cuerpoTabla) {
         let row = cuerpoTabla.insertRow(i);
         let cell1 = row.insertCell(0);
         let cell2 = row.insertCell(1);
         let cell3 = row.insertCell(2);
-        let cell4 = row.insertCell(3); ///////////celda para boton
-        cell1.innerHTML = planes[i].plan300;
-        cell2.innerHTML = planes[i].plan600;
-        cell3.innerHTML = planes[i].plan900;
-                                                    ////////////////////boton
-        let btn = document.createElement("button");   
-        btn.innerHTML = "Borrar fila";                   
-        cell4.appendChild(btn);
-}   }   }
-console.log(planes);
+        let cell4 = row.insertCell(3);
+        cell1.innerHTML = json2.salud8[i].thing.plan300;
+        cell2.innerHTML = json2.salud8[i].thing.plan600;
+        cell3.innerHTML = json2.salud8[i].thing.plan900;
+        let btn1 = document.createElement("button");   
+        btn1.innerHTML = "Borrar fila";                   
+        btn1.classList.add("btnBorrarFila");
+        cell4.appendChild(btn1);
+        let btn2 = document.createElement("button");   
+        btn2.innerHTML = "Editar fila";                   
+        btn2.classList.add("btnEditarFila");
+        cell4.appendChild(btn2);
+      }
+    }
+  }
+  
+  
+  function BtnEditarFila(){
 
-function borrarjson() {
+    let botones = document.querySelectorAll(".btnEditarFila");
+    console.log(botones.length);
+    for (let i = 0 ; i < botones.length ; i++) {
+      botones[i].onclick = function() {
+      console.log("lo edite");
+      let elemento = this.name = i;
+      editarFila(elemento);
+}  }
+}
+  function BtnBorrarFila(){
+
+      let botones = document.querySelectorAll(".btnBorrarFila");
+      console.log(botones.length);
+      for (let i = 0 ; i < botones.length ; i++) {
+        botones[i].onclick = function() {
+        console.log("lo borre");
+        let elemento = this.name = i;
+        eliminarFila(elemento);
+        console.log("elemento =" + elemento);
+  }  }
+}
+function crearInput(){
+  let btnEditar = document.createElement("button");   
+  btnEditar.innerHTML = "Enviar edicion";                   
+  btnEditar.classList.add("btnEnviarEdicionFila");
+  let divEditar = document.querySelector(".Editar");
+        divEditar.appendChild(btnEditar);
+
+  let inputE300 = document.createElement("input");  
+  inputE300.classList.add(".editar300");
+  inputE300.type = "text";
+  divEditar.appendChild(inputE300);
+  let inputE600 = document.createElement("input");  
+  inputE600.classList.add(".editar600");
+  inputE600.type = "text";
+  divEditar.appendChild(inputE600);
+  let inputE900 = document.createElement("input");  
+  inputE900.classList.add(".editar900");
+  inputE900.type = "text";
+  divEditar.appendChild(inputE900);
+}
+
+async function editarFila(i){
+  crearInput();
+  let NuevosPlanes = {
+    "thing": {
+    "plan300": document.querySelector(".editar300").value,
+    "plan600": document.querySelector(".editar600").value,
+    "plan900": document.querySelector(".editar900").value,
+    }
+  }
+  let url = "http://web-unicen.herokuapp.com/api/groups/07/salud8/";
+    let respuesta1 = await fetch(url);
+    let jsonR = await respuesta1.json();
+     let r = jsonR.salud8[i]._id;
+      console.log(r);
+      url = "http://web-unicen.herokuapp.com/api/groups/07/salud8/" + r;
+      r = await fetch(url, {
+        "method": "PUT",
+        "headers": { "Content-Type": "application/json" },
+        "body": JSON.stringify(NuevosPlanes)});
+        
+      document.getElementById("CuerpoTabla").deleteRow(i);
+     
+  console.log("click editar");
+}
+  async function eliminarFila(i){
+    let url = "http://web-unicen.herokuapp.com/api/groups/07/salud8/";
+  
+    let respuesta1 = await fetch(url);
+    let jsonR = await respuesta1.json();
+     let r = jsonR.salud8[i]._id;
+      console.log(r);
+      url = "http://web-unicen.herokuapp.com/api/groups/07/salud8/" + r;
+      r = await fetch(url, {
+        "method": "DELETE",
+        "headers": { "Content-Type": "application/json" },
+
+      });
+      document.getElementById("CuerpoTabla").deleteRow(i);
+    
+    console.log("click borrar");
+  }
+
+  async function borrarjson() {
     let CantFilas = cuerpoTabla.rows.length;
     if (CantFilas > 0) {
-        for (let i = 0; i < CantFilas; i++) {
-            document.getElementById("CuerpoTabla").deleteRow(0);
+      for (let i = 0; i < CantFilas; i++) {
+        document.getElementById("CuerpoTabla").deleteRow(0);
 
-        }
-        planes.splice(0, planes.length);
-        console.log(planes);
-}   }
-function AgregarValores() {
+      }
+    }
+    let url = "http://web-unicen.herokuapp.com/api/groups/07/salud8";
+
+
+    try {
+      let respuesta1 = await fetch(url);
+      let jsonR = await respuesta1.json();
+      console.log(jsonR);
+      for (let elem of jsonR.salud8) {
+        let r = elem._id;
+        console.log(r);
+        url = "http://web-unicen.herokuapp.com/api/groups/07/salud8/" + r;
+
+        r = await fetch(url, {
+          "method": "DELETE",
+          "headers": { "Content-Type": "application/json" },
+
+        });
+      }
+    }
+    catch (e) {
+      console.log(e)
+    }
+   }
+  async function AgregarValores() {
     let NuevosPlanes = {
+      "thing": {
         "plan300": document.querySelector("#input300").value,
         "plan600": document.querySelector("#input600").value,
         "plan900": document.querySelector("#input900").value,
+      }
     }
-    planes.push(NuevosPlanes);
-    CrearCeldas(planes.length - 1, planes.length);
-    console.log(NuevosPlanes);
-    console.log(planes);
-}
 
-function Agregar3Valores() {
-    for (let i = 0; i < 3; i++) {
-        let NuevosPlanes = {
-            "plan300": "test300",
-            "plan600": "test600",
-            "plan900": "test900",
-        }
-        planes.push(NuevosPlanes);
+    let r = await fetch('http://web-unicen.herokuapp.com/api/groups/07/salud8',
+      {
+        "method": "POST",
+        "headers": { "Content-Type": "application/json" },
+        "body": JSON.stringify(NuevosPlanes)
+
+
+      });
+    if (r.ok) {
+      let respuesta = await fetch('http://web-unicen.herokuapp.com/api/groups/07/salud8',
+        {
+          "method": "GET",
+          "headers": { "Content-Type": "application/json" },
+        });
+      let json2 = await respuesta.json();
+      CrearCeldas(json2.salud8.length - 1, json2.salud8.length);
+      console.log(NuevosPlanes);
+
     }
-        CrearCeldas(planes.length - 3, planes.length);
-    console.log(planes);
+  }
+
+  async function Agregar3Valores() {
+    for (let i = 0; i < 3; i++) {
+      let NuevosPlanes = {
+        "thing": {
+          "plan300": "test300",
+          "plan600": "test600",
+          "plan900": "test900",
+        }
+      }
+
+
+      let r = await fetch('http://web-unicen.herokuapp.com/api/groups/07/salud8',
+        {
+          "method": "POST",
+          "headers": { "Content-Type": "application/json" },
+          "body": JSON.stringify(NuevosPlanes)
+        }
+
+      )
+      if (r.ok) {
+        let respuesta = await fetch('http://web-unicen.herokuapp.com/api/groups/07/salud8',
+          {
+            "method": "GET",
+            "headers": { "Content-Type": "application/json" },
+          });
+        let json2 = await respuesta.json();
+
+
+        CrearCeldas(json2.salud8.length - 1, json2.salud8.length);
+
+      }
+    }
 }
-/*--------------------------------------------------opcionales--------------------------------------------------------*/
+/*----------------------------------------------opcionales ENTERGA 2--------------------------------------------------*/
 /*--------------------------------------------buscar y resaltar fila--------------------------------------------------*/
 function buscarTabla (palabraBuscada){
     let filas = document.querySelectorAll("tr");
@@ -211,16 +374,6 @@ let btnResaltarBuscado = document.getElementById("btnResaltar");
 /*------------------------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------------------------*/
 /*------------------------------------------------------------------------------------------------------------------*/
-/*---------------------------------------------eliminar en cada fila------------------------------------------------*/
-
-BtnBorrarFila();//borro de a una fila
-function BtnBorrarFila(){
-    let botones = document.querySelectorAll("button");
-    for (let i = 0 ; i < botones.length ; i++) {
-      botones[i].onclick = function() {
-        console.log("precionado");
-
-}   }   }
 
 
 }/*--llave final---*/
